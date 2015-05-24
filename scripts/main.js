df885c2c2ec221ff500b8c229a42305453e335e0
Lan <llsaphire1029@gmail.com>
2015-05-24 15:44:30 -0700

    first dancing dots animation

--- scripts/main.js
+++ scripts/main.js
@@ -1,165 +1,88 @@
 'use strict';
 
-function yingyang(parentNode, floatVal, img) {
-	this.direction = 'left';
-	this.element = document.createElement('div');
-	this.element.className = 'logo';
-	this.element.style.cssFloat = floatVal;
-	this.element.style.backgroundImage = img;
-	//this.element.style.cssClear = 'both';
-	parentNode.appendChild(this.element);
-}
-
-function row()
-{
-	this.element = document.createElement('div');
-	this.element.className = 'row';
-	//this.element.style.backgroundColor = color;
-	document.body.appendChild(this.element);
-}
-
-// Returns a random number between min (inclusive) and max (exclusive)
-function getRandomArbitrary(min, max) {
-  return Math.random() * (max - min) + min;
-}
-
-/*
-window.onload = function(){
-		var startReverse = function(tween){
-			tween.reverse();
-		};
-
-		var startReplay = function(tween){
-			//var duration = getRandomArbitrary(2, 12);
-			//tween.timeScale(duration);
-			tween.play();
-		};
-
-		for(var i=0; i<10; i++)
-		{
-			var aRow = new row;
-			var yingObj = new yingyang(aRow.element, 'left', "url('/images/Ying.png')");
-			var yangObj = new yingyang(aRow.element, 'right', "url('/images/Yang.png')");
-			
-			//var yingDuration = getRandomArbitrary(2, 10);
-			var yingDuration = 3;
-			TweenLite.to(yingObj.element, yingDuration, {left:'45%', onComplete:startReverse, onCompleteParams:["{self}"], onReverseComplete:startReplay, onReverseCompleteParams:["{self}"]});
-			
-			//var yangDuration = getRandomArbitrary(2, 10);
-			var yangDuration = 3;
-			TweenLite.to(yangObj.element, yangDuration, {right:'45%', onComplete:startReverse, onCompleteParams:["{self}"], onReverseComplete:startReplay, onReverseCompleteParams:["{self}"]});	
-		}
-};
-*/
-
-window.onload = function(){
-		var startReverse = function(tween){
-			tween.reverse();
-		};
-
-		var startReplay = function(tween){
-			var duration = getRandomArbitrary(2, 12);
-			tween.timeScale(duration);
-			tween.play();
-		};
-
-		for(var i=0; i<10; i++)
-		{
-			var aRow = new row;
-			var yingObj = new yingyang(aRow.element, 'left', "url('/images/Ying.png')");
-			var yangObj = new yingyang(aRow.element, 'right', "url('/images/Yang.png')");
-			
-			var yingDuration = getRandomArbitrary(2, 10);
-			TweenLite.to(yingObj.element, yingDuration, {left:'45%', onComplete:startReverse, onCompleteParams:["{self}"], onReverseComplete:startReplay, onReverseCompleteParams:["{self}"]});
-			
-			var yangDuration = getRandomArbitrary(2, 10);
-			TweenLite.to(yangObj.element, yangDuration, {right:'45%', onComplete:startReverse, onCompleteParams:["{self}"], onReverseComplete:startReplay, onReverseCompleteParams:["{self}"]});	
-		}
-};
-
-/*external js
-http://cdnjs.cloudflare.com/ajax/libs/gsap/latest/TweenLite.min.js
-http://cdnjs.cloudflare.com/ajax/libs/gsap/latest/plugins/CSSPlugin.min.js
-*/
-/*
 window.onload = function() {
-  var logo = document.getElementById("logo"),
-		    playBtn = document.getElementById("playBtn"),
-		    pauseBtn = document.getElementById("pauseBtn"),
-		    resumeBtn = document.getElementById("resumeBtn"),
-		    reverseBtn = document.getElementById("reverseBtn"),
-		    playFromBtn = document.getElementById("playFromBtn"),
-		    reverseFromBtn = document.getElementById("reverseFromBtn"),
-		    seekBtn = document.getElementById("seekBtn"),
-		    timeScaleSlowBtn = document.getElementById("timeScaleSlowBtn"),
-		    timeScaleNormalBtn = document.getElementById("timeScaleNormalBtn"),
-		    timeScaleFastBtn = document.getElementById("timeScaleFastBtn"),
-        	restartBtn = document.getElementById("restartBtn"),
-		    tween = TweenLite.to(logo, 6, {left:"632px", ease:Linear.easeNone});
-		
-  playBtn.onclick = function() {
-		//Play the tween forward from the current position.
-		//If tween is complete, play() will have no effect
-    tween.play();
-	  }
-	  pauseBtn.onclick = function() {
-		  tween.pause();
-	  }
-	  resumeBtn.onclick = function() {
-		  //Resume playback in current direction.
-		  tween.resume();
-	  }
-	  reverseBtn.onclick = function() {
-		  tween.reverse();
-	  }
-	  playFromBtn.onclick = function() {
-		  //Play from a sepcified time (in seconds).
-		  tween.play(5);
-	  }
-	  reverseFromBtn.onclick = function() {
-		  //Reverse from a specified time (in seconds).
-		  tween.reverse(1);
-	  }
-	  seekBtn.onclick = function() {
-		  //Jump to specificied time (in seconds) without affecting
-		  //whether or not the tween is paused or reversed.
-		  tween.seek(3);
-	  }
-	  timeScaleSlowBtn.onclick = function() {
-	  	//timescale of 0.5 will make the tween play at half-speed (slower).
-		  //Tween will take 12 seconds to complete (normal duration is 6 seconds).
-		  tween.timeScale(0.5);
-	  }
-	  timeScaleNormalBtn.onclick = function() {
-		  //timescale of 1 will make tween play at normal speed.
-		  tween.timeScale(1);
-	  }
-	  timeScaleFastBtn.onclick = function() {
-		  //timescale of 1 will make the tween play at double-speed (faster).
-		  //Tween will take 3 seconds to complete (normal duration is 6 seconds).
-		  tween.timeScale(2);
-	  }
-	  restartBtn.onclick = function() {
-		  //Start playing from a progress of 0.
-		  tween.restart();
-	  }
-}
-*/
+	var parent = document.getElementById('scene1'),
+	//var parent = document.body,
+		element = this.element = document.createElement('div'),
+		colors = ['#61AC27', '#FF6600', '#F44336', '#E91E63', '#9C27B0', '#673AB7', '#3F51B5', '#2196F3', '#03A9F4', '#FF5722'],
+		i = 10,
+		animationOffset = 0,
 
-/*
-window.onload = function(){
+		createDot = function() {
+			var dot = document.createElement('div');
+			element.appendChild(dot);
+			TweenLite.set(dot, {
+				width: dotSize,
+				height: dotSize,
+				x:radius,
+				//backgroundColor: color,
+				borderRadius: '50%',
+				force3D: true,
+				position: 'absolute'
+			});
+			return dot;
+		},
+		dots = [],
+		tl = new TimelineMax({
+			//paused: true
+			repeat: 2,
+			//repeatDeplay: 0.01
+		}),
+		dot, j, dotSize, radius;
 
-	for(var i=0; i<1; i++)
-	{
-		var yingobj = new ying;
+		parent.appendChild(element);
+		TweenLite.set(element, {
+			top: '40%',
+			left: '42%',
+			position: 'relative',
+			overflow: 'visible',
+			perspective: 600
+		});
 
-		if(i%2)
-		{
-			yingobj.direction = 'left';
-			yingobj.element.style.cssFloat = 'right';
+		while (--i > -1) {
+			//dotSize = Math.floor(Math.random()*(22)+10);
+			dotSize = 24;
+			//radius = Math.floor(Math.random()*(32));
+			radius = dotSize/2;
+			for(j=0; j<2; j++)
+			{
+				var colorIndex = Math.floor(Math.random()*(colors.length));
+				dot = createDot();
+				dots.unshift(dot); //Adding the dot to the beginning of the array
+
+				var startPos = 80;
+				var endPos = radius;
+				var transformx = -(radius-(dotSize/2));
+				if(j%2) {
+					startPos = -80;
+					endPos = -radius;
+					transformx = dotSize+(radius-(dotSize/2));
+				}
+
+				tl.fromTo(dot, 1, {
+					scale: 0.005,
+					opacity: 0,
+					ease: Power2.easeOut,
+					x:startPos, 
+					backgroundColor: colors[colorIndex]}, {
+					scale: 1,
+					opacity: 0.5,
+					x: endPos}, 
+					6*animationOffset).to(dot, 2, {
+					transformOrigin: (transformx+"px 50%"),
+					rotation: 180
+					}, 6*animationOffset+1).to(dot, 1, {
+					x:-startPos,
+					ease: Power2.easeOut,
+					scale: 0.05,
+					opacity:0,
+					backgroundColor: colors[j]
+					}, 6*animationOffset+3);
+			}
+			animationOffset += 0.25;
 		}
-		
-		TweenLite.to(yingobj.element, 5, {left:'45%'});
-	}
+
+		if (TweenLite.render) {
+			TweenLite.render(); //trigger the from() tweens' lazy-rendering (otherwise it'd take one tick to render everything in the beginning state, thus things may flash on the screen for a moment initially). There are other ways around this, but TweenLite.render() is probably the simplest in this case.
+		}
 };
-*/
